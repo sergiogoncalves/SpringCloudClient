16fd2c4d23af49ec468fe27116fce211891c8aa1
Sérgio Gonçalves <eu.sergiogoncalves@gmail.com>
2019-06-05 11:49:13 -0400

    Testando

---
+++ src/main/java/br/com/cloudclient/CloudclientApplication.java
@@ -0,0 +1,13 @@
+package br.com.cloudclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudclientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudclientApplication.class, args);
+    }
+
+}
---
+++ src/main/java/br/com/cloudclient/controller/LuckyWordController.java
@@ -0,0 +1,16 @@
+package br.com.cloudclient.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LuckyWordController {
+
+    @Value("${lucky-word}") String luckyWord;
+
+    @GetMapping("/lucky-word")
+    public String showLuckyWord() {
+        return "The lucky word is: " + luckyWord;
+    }
+}
